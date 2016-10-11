08c8684dcfa4f34addb92853f9ab06445fbc197c
Boris Doomer <doomer@localhost.localdomain>
2016-10-12 01:37:19 +0300

    add some unittests

--- symf/tests/AppBundle/Controller/DefaultControllerTest.php
+++ symf/tests/AppBundle/Controller/DefaultControllerTest.php
@@ -9,19 +9,46 @@ class DefaultControllerTest extends WebTestCase
     /**
      * @dataProvider urlProvider
      */
+    public function testMultiply($url, $parameters = array())
+    {
+        $client = self::createClient();
+        $client->request('POST', $url, $parameters);
+        $this->assertEquals('{"result":24}', $client->getResponse()->getContent());
+    }
+    
+    /**
+     * @dataProvider urlErrorProvider
+     */
+    public function testMultiplyError($url, $parameters = array())
+    {
+        $client = self::createClient();
+        $client->request('POST', $url, $parameters);
+        $this->assertContains('error', $client->getResponse()->getContent());
+    }
+    
+    /**
+     * @dataProvider urlProvider
+     */
     public function testPageIsSuccessful($url)
     {
         $client = self::createClient();
         $client->request('POST', $url);
-
         $this->assertTrue($client->getResponse()->isSuccessful());
     }
 
     public function urlProvider()
     {
         return array(
-            array('/symf/web/api/multiplication'),
-            // ...
+            array('/api/multiplication', array('numbera' => 6, 'numberb' => 4)),
+        );
+    }
+    
+    public function urlErrorProvider()
+    {
+        return array(
+            array('/api/multiplication', array('numbera' => 'derp', 'numberb' => 4)),
+            array('/api/multiplication', array('numbera' => 11, 'numberb' => 'derp')),
+            array('/api/multiplication'),
         );
     }
 }
